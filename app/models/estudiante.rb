21c2ea83011296a10e2220797a7e5bfd9bd1c7ab
Octavio Cruz <octavio.cruzr@utem.cl>
2017-07-26 12:00:48 -0400

    actualizacion

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -9,12 +9,18 @@ class Estudiante < ApplicationRecord
   belongs_to :carrera
   belongs_to :priorizacion
   belongs_to :comuna
-  validates :nombres, presence: {:message => "Nombre en blanco \n."}
+
   validates :apellidos, presence: true
   validates :direccion, presence: true
   validates :email, uniqueness: true
+
   validates :rut, uniqueness: true, rut:true
 
+  validates :nombres, presence: {:message => "LLenado Obligatorio"}
+  validates :nombres, format:{with: /\A[a-zA-Z\s]+\z/,message: "Solo acepta letras"}
+  validates :nombres, length: {minimum: 3, maximum: 15, :message => "El nombre debe tener entre 3 y 15 caracteres"}
 
-
+  validates :apellidos, presence: {:message => "LLenado Obligatorio"}
+  validates :apellidos, format:{with: /\A[a-zA-Z\s]+\z/,message: "Solo acepta letras"}
+  validates :apellidos, length: {minimum: 3, maximum: 15, :message => "El nombre debe tener entre 3 y 15 caracteres"}
 end
